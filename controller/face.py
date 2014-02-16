7ba690e62f316a38e8ae0153261f2f2b408a636b
Jon Ducrou <jonducrou@gmail.com>
2014-02-16 12:42:40 -0800

    add other stuff that wasn't committed.

--- controller/face.py
+++ controller/face.py
@@ -4,28 +4,30 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml')
 
+faces = 'fuck you'
 
-def detect(img):
+def detect(img, faces):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if faces == 'fuck you' or skip % 10 == 0:
+       faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.circle(img, (x+w/2, y+h/2), w/2, (0,255,255), -1 )
         cv2.circle(img, (x+w/3, y+h/5), w/10, (0,0,0), -1 )
         cv2.circle(img, (x+2*w/3, y+h/5), w/10, (0,0,0), -1 )
-        cv2.circle(img, (x+w/2, y+3*h/4), w/7, (0,0,0), -1 )
+        cv2.circle(img, (x+w/2, y+3*h/4), w/5, (0,0,0), -1 )
         cv2.circle(img, (x+w/2, y+h/2), w/2, (0,0,0), 3 )
     return img
 
-cap = cv2.VideoCapture('rtsp://192.168.1.221:8554/')
+cap = cv2.VideoCapture('rtsp://192.168.1.87:8554/')
 
 skip = 1
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     skip += 1	
-    if skip%3 == 0:
-      #frame = detect(frame.copy())
-      continue
+    frame = detect(frame, faces)
+      #continue
+    
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
