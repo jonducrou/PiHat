41d808a24ae5aafa113f932a8afc5e12ad6b204a
jonducrou <jonducrou@gmail.com>
2014-02-18 07:41:32 +0000

    Add daemon for gstreamer

---
+++ pi/start-gstreamer.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.1.221 port=5000
+echo "Up!"
