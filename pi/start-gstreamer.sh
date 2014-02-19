66f22b950a1300f63c3fdcd6d934dd1088136ff6
jonducrou <jonducrou@gmail.com>
2014-02-19 07:46:41 +0000

    more minor tweaks

--- pi/start-gstreamer.sh
+++ pi/start-gstreamer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sleep 15
-raspivid -t 0 -h 600 -w 800 -fps 25 -vt -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.1.221 port=5000
+raspivid -hf -t 0 -h 480 -w 640 -fps 25 -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.1.221 port=5000
 echo "Up!"
